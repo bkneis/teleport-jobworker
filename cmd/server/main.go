53dcfb37a3a735fa37aa58c9c1cdeb80b20e4b41
Bryan Kneis <bryan.kneis@veeasystems.com>
2024-07-01 09:59:50 +0100

    Add cmd/server

---
+++ cmd/server/main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"log"
+	"net"
+
+	"github.com/teleport-jobworker/pkg/rpc"
+)
+
+var port = flag.Int("port", 50051, "the port to serve on")
+
+func main() {
+	// Parse CLI args
+	flag.Parse()
+	log.Printf("server starting on port %d...\n", *port)
+	// Setup and run gRPC server
+	s := rpc.NewServer()
+	lis, err := net.Listen("tcp", fmt.Sprintf("localhost:%d", *port))
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+	if err := s.Serve(lis); err != nil {
+		log.Fatalf("failed to serve: %v", err)
+	}
+}
